be007e6cd7adbe5eca0782e7ba8b3ac8d9986290
Kingsten100 <isak9910@gmail.com>
2024-11-12 12:45:40 +0100

    Byggt ut modal och gjort den funktionell så att det inte går att ta bort task utan att slutföra tasken först. Fixat olika buggar.

--- scripti.js
+++ scripti.js
@@ -4,6 +4,9 @@ const taskInput = document.querySelector('#task-input')
 const listContainer = document.querySelector('#list-container')
 const error = document.querySelector('#error')
 
+const modal = document.querySelector('#modal-wrapper')
+const closeModal = document.querySelector('#close-modal')
+const modalWrapper = document.querySelector('#modal-wrapper')
 
 
 async function getTasks(){
@@ -36,9 +39,11 @@ async function addTask(task){
             body: JSON.stringify({title: task})
         })
 
-        const data = await res.json()
+        const newTask = await res.json()
 
-        console.log(data)
+        createListItem(newTask)
+
+        console.log(newTask)
     }
     catch (error) {
         console.log('fel', error)
@@ -91,36 +96,39 @@ async function updateTask(id, completed){
 
 
 // Funktion som lyssnar efter en submit och skickar iväg min uppgift till formuläret
-aaddTask.addEventListener('submit', e => {
+aaddTask.addEventListener('submit', async e => {
     e.preventDefault()
 
-    const task = document.querySelector('#task-input').value
+    const taskTitle = document.querySelector('#task-input').value
 
-    addTask(task)
-    createListItem(task)
-
-    console.log(task)
-    taskInput.value = ''
-    return task
-})
-
-// Funktion som skapar mina uppgifter i html filen
-function createListItem(task) {
-
-    if(task === ''){
+    if(taskTitle === ''){
         if(!document.querySelector('.error')){
-
             let errorMsg = document.createElement('span')
             errorMsg.textContent = 'You need to enter a task'
             errorMsg.classList.add('error')
             error.appendChild(errorMsg)
             console.log(errorMsg)
         }
-       
+            
         
-        return
+        
     } else{
+        
+        error.remove()
+        
+        
+        await addTask(taskTitle)
+    }
 
+    
+    taskInput.value = ''
+    
+})
+
+// Funktion som skapar mina uppgifter i html filen
+function createListItem(task) {
+
+    
         const existingError = document.querySelector('.error');
         if (existingError) {
             existingError.remove();
@@ -136,11 +144,12 @@ function createListItem(task) {
 
     let taskText = document.createElement('span')
     taskText.textContent = task.title;
+    li.appendChild(taskText);
     if(task.completed){
         taskText.classList.add('complete')
     }
     
-    li.appendChild(taskText);
+    
 
     let button = document.createElement('button');
     button.textContent = ' X ';
@@ -150,16 +159,40 @@ function createListItem(task) {
 
     button.addEventListener('click', async e => {
         e.stopPropagation()
-        await deleteTask(id)
-        li.remove();
+        if(!task.completed){
+            modal.style.display = 'block'
+        }
+        else{
+            await deleteTask(id)
+            li.remove();
+        }
+
+
+
+       
     })
 
     li.addEventListener('click', async e => {
+        e.stopPropagation()
+        const taskStatus = !task.completed
         await updateTask(id, !task.completed)
         taskText.classList.toggle('complete')
+        task.completed = taskStatus
     })
 
     listContainer.appendChild(li);
-    }    
+
+    modalWrapper.addEventListener('click', e => {
+        e.stopPropagation()
+        if(e.target === modalWrapper){
+            modal.style.display = 'none'
+        }
+    })
+
+    modal.addEventListener('click', e => {
+        e.stopPropagation()
+        modal.style.display = 'none'
+    })
+        
 }
 
